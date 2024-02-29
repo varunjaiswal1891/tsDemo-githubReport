550fbe6fc2b0778f1035a54303eca0ae7648339b
Varun Jaiswal <varunpandey511@gmail.com>
2024-02-29 10:50:19 +0530

    initialized dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ console.log("hello varun");
 
 //npm init   --generate package.json structure
 //tsc --init --generate tsconfig.json structure
+//npm install request lodash --save 
+//npm install @types/lodash @types/request --save-dev
