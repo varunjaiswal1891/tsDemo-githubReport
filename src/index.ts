ee68ad090cd3280e97d4f07b89ae718a04dee499
Varun Jaiswal <varunpandey511@gmail.com>
2024-03-01 19:01:55 +0530

    updated with cmdLine argument

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import {GithubApiService} from './GithubApiService'; 
 import { Repo } from './Repo';
 import { User } from './User';
+import * as _ from 'lodash';
 
 //npm init   --generate package.json structure
 //tsc --init --generate tsconfig.json structure
@@ -10,12 +11,31 @@ import { User } from './User';
 //npm install @types/lodash @types/request --save-dev
 
 let svc = new GithubApiService();
-svc.getUserInfo("varunjaiswal1891",(user: User) => {
-    //console.log(user);
-    console.log("user object = "+user);
-    console.log("username = "+user.fullName);
+if(process.argv.length <3)
+{
+    console.log("please pass username as an argument");
+}
+else
+{
+    //username = varunjaiswal1891
+  let username = process.argv[2];
+  svc.getUserInfo(username,(user: User) => {
+     svc.getRepos(username,(repos: Repo[]) => {
+
+        let sortedRepos = _.sortBy(repos, [(repo:Repo) => repo.size * -1 ]); // multiply -1 to sort in desc order
+        //let filteredRepos = _.take(sortedRepos,3);
+        user.repos = sortedRepos;
+        console.log("User = "+user.repoCount + ","+ user.fullName);   
+        //console.log("repos ="+ user.repos);
+        for(var i=0;i<user.repoCount;i++)
+        {
+            if(user.repos[i].url != null)
+            {
+                console.log("repo url ="+user.repos[i].url +", size = "+ user.repos[i].size);
+            }
+        }
+       });
+   
 });
 
-svc.getRepos("varunjaiswal1891",(repos: Repo[]) => {
- console.log("Repos = "+repos);   
-});
+}
